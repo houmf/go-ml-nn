b834da10707416953bbf8ee8bde818e2892278a5
dimmu <dathanasakis@gmail.com>
2016-10-10 09:01:31 +0100

    reorganizing code

--- volume.go
+++ volume.go
@@ -1,15 +1,44 @@
-package layer 
-
+package main
+import "fmt"
+import "strconv"
 //Volume is a basic three-dimensional Volume passed between layers
 //dimensionality is MxNxD
 type Volume struct{
     m,n,d int
-    data [][][]float
+    data [][][]float64
 }
 
 //create volume with specific size
+func Init(m int,n int,d int) Volume{
+  V:=Volume{m,n,d,nil}
+  V.data =make([][][]float64, d)
+  for i:=0;i<d;i++{
+    V.data[i]=make([][]float64,m)
+    for j:=0; j<m;j++{
+      V.data[i][j]=make([]float64,n)
+    }
+  }
+  return V
+}
+
+//print contents of a volume
+func (V Volume) String() string{
+  rtn:=""
+  for i:=0;i<V.d;i++{
+    for j:=0;j<V.m;j++{
+      rtn += strconv.Itoa(i)+": "+strconv.Itoa(j)+": "
+      for k:=0;k<V.m;k++{
+        rtn+=strconv.FormatFloat(V.data[i][j][k],'e',-1,64)+" "
+      }
+      rtn+="\n"
+    }
+  }
+  return rtn
+}
 
 //add to volume in place
+
+//func (V Volume)Adj(V)
 
 //add two volumes
 
@@ -18,4 +47,8 @@ type Volume struct{
 //scale a volume
 
 //return a new volume after applying function f on it
-
+func main(){
+  V:=Init(2,2,2)
+  //V.d=2
+  fmt.Println("YO BABY!",V)
+}
